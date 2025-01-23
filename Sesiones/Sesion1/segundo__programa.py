32adb07c3694af124fc8f22100c31bc3f514a6ec
DarioVillamarin <dario_villamarin@hotmail.com>
2025-01-23 11:27:46 -0500

    Añddidos primeros programas a la sesion1

---
+++ Sesiones/Sesion1/segundo__programa.py
@@ -0,0 +1,10 @@
+#solicita datos al usuario
+
+nombre= input("Cuál es tu nombre: ")
+edad= int(input("¿Cuál es tu edad?: "))
+años_futuros=edad+5
+
+
+#imprimier datos capturados
+
+print(f"hola {nombre}, tienes {edad} años, en 5 años tendrás {años_futuros}")
